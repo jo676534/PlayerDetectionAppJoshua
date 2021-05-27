5e7348dcfe417a2d8711f948e22818323081d5f2
IamMarkT <markt1084@yahoo.com>
2021-05-27 18:13:11 -0400

    added video editing components

--- apps/video_edit.py
+++ apps/video_edit.py
@@ -1,13 +1,12 @@
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 import plotly.express as px
 import pandas as pd
 import pathlib
-import ffmpeg_streaming
-import dash_player
-
-from moviepy.editor import *
+# import ffmpeg_streaming
+# import dash_player
+# from moviepy.editor import *
 from app import app
 
 import cv2
@@ -21,11 +20,47 @@ import numpy as np
 layout = html.Div(children=[
 
     html.H1('VIDEO'),
-    # dash_player.DashPlayer(url="https://trinidaddemobucket.s3.amazonaws.com/Sample+Soccer+Video.mp4",
-    #                        controls=True,
-    #                        ),
 
+
+
+
+
+    # dash_player.DashPlayer(
+    #     id='video',
+    #     url="https://trinidaddemobucket.s3.amazonaws.com/Sample+Soccer+Video.mp4",
+    #     controls=True,
+    # ),
+    # html.Div(id='div-current-time', children=[]),
+    # html.Div(id='div-method-output', children=[]),
+
+
+
+    # html.Button('Set seekTo to 10', id='button-seek-to'),
+    dcc.Input(id="startingTime", type="number", debounce=True,
+              placeholder="Enter Trim Start Time"),
+    dcc.Input(id="EndingTime", type="number", debounce=True,
+              placeholder="Enter Trim End Time"),
 ])
 # pull video from s3 bucket ffmpeg_streaming
 # give queries to where to clip video
 # reupload video as copy but keep the original
+
+
+# @app.callback(Output('div-current-time', 'children'),
+#               [Input('video', 'currentTime')])
+# def update_time(currentTime):
+#     print(currentTime)
+#     return 'Current Time: {}'.format(currentTime)
+
+
+# @app.callback(Output('div-method-output', 'children'),
+#               [Input('video', 'secondsLoaded')],
+#               [State('video', 'duration')])
+# def update_methods(secondsLoaded, duration):
+#     return 'Second Loaded: {}, Duration: {}'.format(secondsLoaded, duration)
+
+
+# @app.callback(Output('video', 'seekTo'),
+#               [Input('button-seek-to', 'n_clicks')])
+# def set_seekTo(n_clicks):
+#     return 10
