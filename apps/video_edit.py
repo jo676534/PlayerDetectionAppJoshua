62125eef1a16bbbbd93e9d710ee6b2dd2074e94a
IamMarkT <markt1084@yahoo.com>
2021-05-27 14:34:23 -0400

    fixed to allow master to run

--- apps/video_edit.py
+++ apps/video_edit.py
@@ -10,28 +10,8 @@ import dash_player
 from moviepy.editor import *
 from app import app
 
-
 import cv2
 import numpy as np
-
-cap = cv2.VideoCapture('Sample Soccer Video.mp4')
-frameCount = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-frameWidth = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-frameHeight = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
-buf = np.empty((frameCount, frameHeight, frameWidth, 3), np.dtype('uint8'))
-
-fc = 0
-ret = True
-
-while (fc < frameCount  and ret):
-    ret, buf[fc] = cap.read()
-    fc += 1
-
-cap.release()
-
-
-cv2.waitKey(0)
 
 # clip = VideoFileClip("Sample Soccer Video.mp4")
 # video = ffmpeg_streaming.input('./Sample Soccer Video.mp4')
@@ -44,9 +24,7 @@ layout = html.Div(children=[
     # dash_player.DashPlayer(url="https://trinidaddemobucket.s3.amazonaws.com/Sample+Soccer+Video.mp4",
     #                        controls=True,
     #                        ),
-    dash_player.DashPlayer(url=buf,
-                           controls=True,
-                           ),
+
 ])
 # pull video from s3 bucket ffmpeg_streaming
 # give queries to where to clip video
