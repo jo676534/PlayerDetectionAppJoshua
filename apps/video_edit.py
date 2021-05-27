39fe92a913fee564199f6c44ac1fa827bd598911
IamMarkT <markt1084@yahoo.com>
2021-05-27 18:21:50 -0400

    added components to video editing

--- apps/video_edit.py
+++ apps/video_edit.py
@@ -1,37 +1,16 @@
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 import plotly.express as px
 import pandas as pd
 import pathlib
-import ffmpeg_streaming
+# import ffmpeg_streaming
 import dash_player
-
-from moviepy.editor import *
+# from moviepy.editor import *
 from app import app
-
 
 import cv2
 import numpy as np
-
-cap = cv2.VideoCapture('Sample Soccer Video.mp4')
-frameCount = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-frameWidth = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-frameHeight = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
-buf = np.empty((frameCount, frameHeight, frameWidth, 3), np.dtype('uint8'))
-
-fc = 0
-ret = True
-
-while (fc < frameCount  and ret):
-    ret, buf[fc] = cap.read()
-    fc += 1
-
-cap.release()
-
-
-cv2.waitKey(0)
 
 # clip = VideoFileClip("Sample Soccer Video.mp4")
 # video = ffmpeg_streaming.input('./Sample Soccer Video.mp4')
@@ -41,13 +20,49 @@ cv2.waitKey(0)
 layout = html.Div(children=[
 
     html.H1('VIDEO'),
-    # dash_player.DashPlayer(url="https://trinidaddemobucket.s3.amazonaws.com/Sample+Soccer+Video.mp4",
-    #                        controls=True,
-    #                        ),
-    dash_player.DashPlayer(url=buf,
-                           controls=True,
-                           ),
+
+
+
+
+
+    dash_player.DashPlayer(
+        id='video',
+        url="https://trinidaddemobucket.s3.amazonaws.com/Sample+Soccer+Video.mp4",
+        controls=True,
+    ),
+    html.Button('Set seekTo to 10', id='button-seek-to'),
+
+    html.Div(id='div-current-time', children=[]),
+    html.Div(id='div-method-output', children=[]),
+
+
+
+    # html.Button('Set seekTo to 10', id='button-seek-to'),
+    dcc.Input(id="startingTime", type="number", debounce=True,
+              placeholder="Enter Trim Start Time"),
+    dcc.Input(id="EndingTime", type="number", debounce=True,
+              placeholder="Enter Trim End Time"),
 ])
 # pull video from s3 bucket ffmpeg_streaming
 # give queries to where to clip video
 # reupload video as copy but keep the original
+
+
+@app.callback(Output('div-current-time', 'children'),
+              [Input('video', 'currentTime')])
+def update_time(currentTime):
+    print(currentTime)
+    return 'Current Time: {}'.format(currentTime)
+
+
+@app.callback(Output('div-method-output', 'children'),
+              [Input('video', 'secondsLoaded')],
+              [State('video', 'duration')])
+def update_methods(secondsLoaded, duration):
+    return 'Second Loaded: {}, Duration: {}'.format(secondsLoaded, duration)
+
+
+@app.callback(Output('video', 'seekTo'),
+              [Input('button-seek-to', 'n_clicks')])
+def set_seekTo(n_clicks):
+    return 10
