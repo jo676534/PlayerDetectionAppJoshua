045aa01ff3ab51c3e8163064afab79cf2154f91b
dianabisbe <68605289+dianabisbe@users.noreply.github.com>
2021-07-16 13:54:18 -0400

    Style Changes

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 import dash_html_components as html
 
-app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP, 'https://codepen.io/chriddyp/pen/bWLwgP.css'],
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP],
                 meta_tags=[{'name': 'viewport',
                             'content': 'width=device-width, initial-scale=1.0'}],
                             suppress_callback_exceptions = True
