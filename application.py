302a09a3f69cbbda26648425c4afeca13524fb73
IamMarkT <markt1084@yahoo.com>
2021-08-02 16:33:23 -0400

    deleted final_review

--- application.py
+++ application.py
@@ -14,8 +14,6 @@ from apps import initial_review
 from apps import video_edit
 from apps import dashboard
 from apps import add_track
-from apps import final_review
-
 
 
 
@@ -28,7 +26,6 @@ navbar = dbc.NavbarSimple(
                 dbc.DropdownMenuItem(dcc.Link('Video Editor', href='/apps/video_edit')),
                 dbc.DropdownMenuItem(dcc.Link('Dashboard', href='/apps/dashboard')),
                 dbc.DropdownMenuItem(dcc.Link('Add Track', href='/apps/add_track')),
-                dbc.DropdownMenuItem(dcc.Link('Final Review', href='/apps/final_review')),
             ],
             nav=True,
             in_navbar=True,
@@ -74,8 +71,6 @@ def display_page(pathname):
         return dashboard.layout
     if pathname == '/apps/add_track':
         return add_track.layout
-    if pathname == '/apps/final_review':
-        return final_review.layout
     else:
         return home.layout
 
